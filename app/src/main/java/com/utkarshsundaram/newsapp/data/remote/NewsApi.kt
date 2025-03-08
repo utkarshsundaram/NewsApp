ef11d33d73a4b49328297d7af1742dbc2f4eb450
Utkarsh Sundaram <ustheleo@gmail.com>
2025-03-08 12:58:12 +0530

    feat: Use BuildConfig to store API_KEY

--- app/src/main/java/com/utkarshsundaram/newsapp/data/remote/NewsApi.kt
+++ app/src/main/java/com/utkarshsundaram/newsapp/data/remote/NewsApi.kt
@@ -1,5 +1,6 @@
 package com.utkarshsundaram.newsapp.data.remote
 
+import com.utkarshsundaram.newsapp.BuildConfig
 import com.utkarshsundaram.newsapp.data.model.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -9,6 +10,6 @@ interface NewsApi {
     @GET("top-headlines")
     suspend fun getTopHeadlines(
         @Query("country") country: String = "us",
-        @Query("apiKey") apiKey: String
+        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY
     ): Response<NewsResponse>
 }
